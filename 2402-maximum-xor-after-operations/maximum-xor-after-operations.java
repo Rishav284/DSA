62c067ac7395b9cd639cd911e64ef5897beb11e2
Rishav Kumar <rk2845897@gmail.com>
2026-07-30 00:43:21 +0530

    Time: 1 ms (100.00%) | Memory: 59.3 MB (84.03%) - LeetSync

--- 2402-maximum-xor-after-operations/maximum-xor-after-operations.java
+++ 2402-maximum-xor-after-operations/maximum-xor-after-operations.java
@@ -1,21 +1,9 @@
 class Solution {
     public int maximumXOR(int[] nums) {
-        
-        //to count frequency of ith bit
-        int count[]=new int[32];
-        
-        for(int num:nums){
-            for(int i=0; i<32;i++){
-                 count[i] += num>>i&1;
-             }
-        }
-        
-        int ans = 0;
-        for(int i=0; i<32; i++){
-            //if count of ith bit is greater than 0, we can make xor = 1, so it can contribute to solution
-            if(count[i]>0)
-            ans |= 1<<i;
-        }
-        return ans;
+       int ans = 0;
+       for(int ele : nums){
+        ans |=ele;
+       } 
+       return ans;
     }
 }
