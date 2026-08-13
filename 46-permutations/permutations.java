c1f2352eae24dd290d57a2c62be473530144f68f
Rishav Kumar <rk2845897@gmail.com>
2026-08-13 21:50:34 +0530

    Time: 1 ms (87.34%) | Memory: 45 MB (98.66%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,25 +2,26 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         int n=nums.length;
         List<List<Integer>> ans=new ArrayList<>();
-        boolean[] taken= new boolean[n];
-        Arrays.fill(taken,false);
         ArrayList<Integer> ds=new ArrayList<>();
-        backtrack(ans,nums,taken,ds);
+        backtrack1(0,nums,ds,ans);
         return ans;
     }
-    static void backtrack(List<List<Integer>> ans,int[] nums,boolean[] taken,ArrayList<Integer> ds){
+    static void backtrack1(int currInd,int[] nums,ArrayList<Integer> ds,List<List<Integer>> ans){
         if(ds.size()==nums.length){
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!taken[i]){
-                taken[i]=true;
-                ds.add(nums[i]);
-                backtrack(ans,nums,taken,ds);
-                taken[i]=false;
-                ds.remove(ds.size()-1);
-            }
+        for(int i=currInd;i<nums.length;i++){
+            swap(currInd,i,nums);
+            ds.add(nums[currInd]);
+            backtrack1(currInd+1,nums,ds,ans);
+            ds.remove(ds.size()-1);
+            swap(i,currInd,nums);
         }
+    }
+    static void swap(int l,int r,int[] nums){
+        int temp=nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
     }
 }
