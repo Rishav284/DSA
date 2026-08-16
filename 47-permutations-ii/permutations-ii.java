df037877a78ddc338eab79e3af1e882c343b49ed
Rishav Kumar <rk2845897@gmail.com>
2026-08-16 21:51:58 +0530

    Time: 3 ms (91.97%) | Memory: 46.9 MB (29.54%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        int n=nums.length;
-        List<List<Integer>> ans=new ArrayList<>();
-        boolean[] taken= new boolean[n];
-        Arrays.fill(taken,false);
-        ArrayList<Integer> ds=new ArrayList<>();
-        backtrack(ans,nums,taken,ds);
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] taken = new boolean[n];
+        ArrayList<Integer> ds = new ArrayList<>();
+        backtrack(ans, nums, taken, ds);
         return ans;
     }
-    static void backtrack(List<List<Integer>> ans,int[] nums,boolean[] taken,ArrayList<Integer> ds){
-        if(ds.size()==nums.length){
-            if(!ans.contains(ds)) ans.add(new ArrayList<>(ds));
+
+    static void backtrack(List<List<Integer>> ans, int[] nums, boolean[] taken, ArrayList<Integer> ds) {
+        if (ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!taken[i]){
-                taken[i]=true;
-                ds.add(nums[i]);
-                backtrack(ans,nums,taken,ds);
-                taken[i]=false;
-                ds.remove(ds.size()-1);
-            }
+        for (int i = 0; i < nums.length; i++) {
+            if (taken[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !taken[i - 1]) continue;
+            taken[i] = true;
+            ds.add(nums[i]);
+            backtrack(ans, nums, taken, ds);
+            taken[i] = false;
+            ds.remove(ds.size() - 1);
         }
     }
 }
