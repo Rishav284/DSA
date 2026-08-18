d93eafd5bc798bb7999d79c4cb5b0eae70cc39bb
Rishav Kumar <rk2845897@gmail.com>
2026-08-18 18:05:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (48.88%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -3,22 +3,20 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         if(k*(k+1)/2>n) return result;
         List<Integer> ds=new ArrayList<>();
-        backtrack(0,k,1,n,ds,result);
+        backtrack(k,1,n,ds,result);
         return result;
     }
-    static void backtrack(int currSum, int k, int currElement,int n, List<Integer> ds, List<List<Integer>> result) {
-        if(currSum>n) return;
+    static void backtrack(int k, int currElement, int n, List<Integer> ds, List<List<Integer>> result) {
         if(ds.size()==k){
-            if(currSum==n && !result.contains(new ArrayList<>(ds))){
+            if(n==0){
                 result.add(new ArrayList<>(ds));
             }
             return;
         }
         for(int i=currElement;i<=9;i++){
             ds.add(i);
-            backtrack(currSum+i,k,i+1,n,ds,result);
+            backtrack(k,i+1,n-i,ds,result);
             ds.remove(ds.size()-1);
-            backtrack(currSum,k,i+1,n,ds,result);
         }
     }
 }
