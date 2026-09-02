1d96cb7871af6bfa0fb7445621f15aad67a77779
Rishav Kumar <rk2845897@gmail.com>
2026-09-02 23:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (45.96%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,30 +1,24 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0 || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
 
-        // Initialize DP table to memoize results
-        int[][] dp = new int[m][n];
-        for (int[] row : dp) Arrays.fill(row, -1);
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
 
-        // Return the total number of paths
-        return func(m - 1, n - 1, matrix, dp);
-    }
-    
-    static int func(int i, int j, int[][] matrix, int[][] dp) {
-        // Base cases
-        if (i < 0 || j < 0 || matrix[i][j] == 1) return 0;
-        else if (i == 0 && j == 0) return 1;
+        int[] previous = new int[n];
+        int[] current = new int[n];
+        previous[0] = 1;
 
-        // If the result is already computed, return it
-        if (dp[i][j] != -1) return dp[i][j];
+        for (int i = 0; i < m; i++) {
+            current[0] = obstacleGrid[i][0] == 1 ? 0 : previous[0];
+            for (int j = 1; j < n; j++) {
+                current[j] = obstacleGrid[i][j] == 1 ? 0 : current[j-1] + previous[j];
+            }
+            System.arraycopy(current, 0, previous, 0, n);
+        }
 
-        /* Calculate the number of ways by
-        moving up and left recursively. */
-        int up = func(i - 1, j, matrix, dp);
-        int left = func(i, j - 1, matrix, dp);
-
-        // Return the total ways
-        return dp[i][j] = up + left;
+        return previous[n-1];
     }
 }
