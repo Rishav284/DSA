8636fdc45805f68f84c4aaf1cd281e9d4ae427ef
Rishav Kumar <rk2845897@gmail.com>
2026-09-02 16:01:59 +0530

    Time: 1 ms (14.07%) | Memory: 41.8 MB (96.03%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,20 +1,25 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int cR=m;
-        int cC=n;
         int[][] dp=new int[m][n];
-        return memoization(cR,cC,m,n,dp);
+        return tabulation(m,n,dp);
     }
 
-    static int memoization(int r,int c,int m,int n,int[][] dp){
-        if(r==1 && c==1){
-            return 1;
+    static int tabulation(int m, int n, int[][] dp) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+                dp[i][j] = up + left;
+            }
         }
-        if(dp[r-1][c-1]!=0) return dp[r-1][c-1];
-        int down=0;
-        int right=0;
-        if(r>1) down=memoization(r-1,c,m,n,dp);
-        if(c>1) right=memoization(r,c-1,m,n,dp);
-        return dp[r-1][c-1]=down+right;
+        return dp[m - 1][n - 1];
     }
 }
